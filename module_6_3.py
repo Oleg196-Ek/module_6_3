7f3ff4509b503ba0600eb0e036e83342f6ae1d39
Oleg196-Ek <zhookov@mail.ru>
2024-11-13 18:11:13 +0300

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -3,54 +3,51 @@ class Animal:
     live = True
     sound = None
     _DEGREE_OF_DANGER = 0
-    def __init__(self, speed):
+    def __init__(self):
         self._cords = [0,0,0]
-        self.speed = speed
+        self.speed = 0
     def move(self, dx, dy, dz):
-        if sum (self._cords) + dz < 0:
+        x = self._cords[0] + dx * self.speed
+        y = self._cords[1] + dy * self.speed
+        z = self._cords[2] + dz * self.speed
+        if z < 0:
             print("It's too deep, i can't dive :(" )
-        self._cords[0] += dx * self.speed
-        self._cords[1] += dy * self.speed
-        self._cords[2] += dz * self.speed
+        else:
+            self._cords = [x, y, z]
 
     def get_cords(self):
         print (f'{self._cords[0]},{self._cords[1]}, {self._cords[2]}')
     def attack(self):
         if self._DEGREE_OF_DANGER <5:
             print(f'Sorry, i`m peaceful :)')
-
         else:
             print (f'Be careful, i`m attacking you 0_0')
 class Bird(Animal):
     beak = True
-    def __init__(self,speed):
-        super().__init__(speed)
+
     def lay_eggs(self):
         number = random.randint(1,4)
         print(f'Here are(is) {number} eggs for you')
-class AquaticAnimal (Animal):
+class AquaticAnimal(Animal):
     _DEGREE_OF_DANGER = 3
-    def __init__(self,speed):
-        super().__init__(speed)
+
     def dive_in(self, dz):
-        dz = abs(dz)
-        new_dz = self._cords[2] - (dz/2) * self.speed
-        if new_dz < 0:
-            print("It's too deep, i can't dive :(" )
-        else:
-            self._cords[2] = new_dz
+        dive_z = abs(dz) // 2 * self.speed
+        self._cords[2] -= dive_z
+
 class PoisonousAnimal(Animal):
     _DEGREE_OF_DANGER = 8
-    def __init__(self, speed):
-        super().__init__(speed)
+
 class Duckbill ( Bird, AquaticAnimal, PoisonousAnimal):
     sound = "Click-click-click"
+
     def __init__(self, speed):
-        Bird.__init__(self,speed)
-        AquaticAnimal.__init__(self, speed)
-        PoisonousAnimal.__init__(self, speed)
+        super().__init__()
+        self.speed = speed
+
     def speak(self):
-        print(self.sound)
+        if self.sound is not None:
+            print(self.sound)
 
 db = Duckbill(10)
 
